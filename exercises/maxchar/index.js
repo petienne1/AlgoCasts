0e6a7e585392094a4ec75cee71a3c841881332f0
Pierry Etienne <pierry.etienne@gmail.com>
2019-12-10 20:22:19 -0500

    max characters solution

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,24 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  let obj = {};
+  let max = 0;
+  let maxCharacter = ''
+  for (let i = 0; i < str.length; i++) {
+    if(obj[str[i]] === undefined) {
+      obj[str[i]] = 1;
+    } else {
+      obj[str[i]]++;
+    }
+  }
+  for(let prop in obj) {
+    if(obj[prop] > max) {
+      max = obj[prop];
+      maxCharacter = prop;
+    }
+  }
+  return maxCharacter;
+}
 
 module.exports = maxChar;
