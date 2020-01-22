cae81033bba42a609b70ff98b0745f5b09d4d95e
Pierry Etienne <pierry.etienne@gmail.com>
2020-01-22 17:47:14 -0500

    vowels solution

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,17 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) {
+  let count = 0;
+  let letters = ['a', 'e', 'i', 'o', 'u'];
+  let str1 = str.toLowerCase();
+
+  for (let i = 0; i < str1.toLowerCase().length; i++) {
+    if (letters.includes(str1[i])) {
+      count++;
+    }
+  }
+  return count;
+}
 
 module.exports = vowels;
