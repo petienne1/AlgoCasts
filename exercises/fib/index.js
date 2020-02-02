56faea947dabf4efbcce548a6f040f5866a3b79b
Pierry Etienne <pierry.etienne@gmail.com>
2020-02-02 10:04:57 -0500

    fibonacci solution

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,6 +8,18 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+function fib(n) {
+  // create an array with 0,1 inside of it
+  // create a for loop where i equals 2 and break the loop when i equals n
+    // push sum of two previous numbers into array
+  // return array[n]
+
+  let fibonacci = [0,1];
+
+  for(let i = 2; i <= n; i++) {
+    fibonacci.push(fibonacci[i-2] + fibonacci[i-1]);
+  }
+  return fibonacci[n];
+}
 
 module.exports = fib;
